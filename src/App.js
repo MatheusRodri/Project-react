32b96dfa683efe9ce8e5e8e643c9aa95bf3302f1
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2021-02-02 16:53:12 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BoasVindas from './components/BoasVindas'
+import Aviso from './components/Aviso'
 function App() {
   return (
     <div className="App">
      <h1>Hello World !!!</h1>
      <BoasVindas/>
+     <Aviso/>
     </div>
   );
 }
---
+++ src/components/Aviso/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './aviso.css'
+export default function Aviso(){
+ return(
+     <h5 className="aviso">ESSE SITE ESTÁ EM DESENVOLVIMENTO</h5>
+ )
+}
